264aa4eca2bc6fa14b4077790ebca8613af1d534
Morten Faarkrog <morten@adaptmobile.dk>
2017-07-17 10:13:54 +0200

    Added support for UIBarButtonItem

---
+++ Apply/Classes/Extensions/UIBarButtonItem+Apply.swift
@@ -0,0 +1,68 @@
+//
+//  UIBarButtonItem+Apply.swift
+//  Pods
+//
+//  Created by Morten Faarkrog on 7/17/17.
+//
+//
+
+import UIKit
+
+public extension UIBarButtonItem {
+  
+  @discardableResult public func apply<T: UIBarButtonItem>(action: Selector?) -> T {
+    self.action = action
+    return self as! T
+  }
+  
+  @discardableResult public func apply<T: UIBarButtonItem>(style: UIBarButtonItemStyle) -> T {
+    self.style = style
+    return self as! T
+  }
+  
+  @discardableResult public func apply<T: UIBarButtonItem>(possibleTitles: Set<String>?) -> T {
+    self.possibleTitles = possibleTitles
+    return self as! T
+  }
+  
+  @discardableResult public func apply<T: UIBarButtonItem>(width: CGFloat) -> T {
+    self.width = width
+    return self as! T
+  }
+  
+  @discardableResult public func apply<T: UIBarButtonItem>(customView: UIView?) -> T {
+    self.customView = customView
+    return self as! T
+  }
+  
+  @discardableResult public func apply<T: UIBarButtonItem>(tintColor: UIColor?) -> T {
+    self.tintColor = tintColor
+    return self as! T
+  }
+  
+  @discardableResult public func apply<T: UIBarButtonItem>(backButtonBackgroundImage: UIImage, for controlState: UIControlState, barMetric: UIBarMetrics) -> T {
+    self.setBackButtonBackgroundImage(backButtonBackgroundImage, for: controlState, barMetrics: barMetric)
+    return self as! T
+  }
+  
+  @discardableResult public func apply<T: UIBarButtonItem>(backButtonTitlePositionAdjustment adjustment: UIOffset, for barMetric: UIBarMetrics) -> T {
+    self.setBackButtonTitlePositionAdjustment(adjustment, for: barMetric)
+    return self as! T
+  }
+  
+  @discardableResult public func apply<T: UIBarButtonItem>(backButtonBackgroundVerticalPositionAdjustment adjustment: CGFloat, for barMetric: UIBarMetrics) -> T {
+    self.setBackgroundVerticalPositionAdjustment(adjustment, for: barMetric)
+    return self as! T
+  }
+  
+  @discardableResult public func apply<T: UIBarButtonItem>(backgroundImage: UIImage?, for controlState: UIControlState, barMetric: UIBarMetrics, style: UIBarButtonItemStyle) -> T {
+    self.setBackgroundImage(backgroundImage, for: controlState, style: style, barMetrics: barMetric)
+    return self as! T
+  }
+  
+  @discardableResult public func apply<T: UIBarButtonItem>(titlePositionAdjustment adjustment: UIOffset, for barMetric: UIBarMetrics) -> T {
+    self.setTitlePositionAdjustment(adjustment, for: barMetric)
+    return self as! T
+  }
+  
+}
